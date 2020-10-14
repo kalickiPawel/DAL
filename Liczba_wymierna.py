3e965004e7f07a538493ce7e9828c2372704dbae
Paweł Kalicki <kalicki.paul@icloud.com>
2020-10-14 18:42:15 +0200

    Added comparing numbers

--- Liczba_wymierna.py
+++ Liczba_wymierna.py
@@ -1,14 +1,13 @@
+import math
+
 class Liczba_wymierna:
     def __init__(self, integer_part, numerator, denominator):
-        
-        if denominator < 0:
+
+        if denominator <= 0 and numerator !=0:
             raise ValueError("Mianownik musi byc dodatni")
-        elif denominator == 0:
-            if numerator == 0:
-                n = None
-                d = None
-            else:
-                raise ValueError("Licznik musi byc dodatni")
+
+        if integer_part and numerator < 0:
+            raise ValueError("Licznik musi byc dodatni gdy wystepuje liczba calkowita")
         
         if integer_part:
             if integer_part > 0:
@@ -20,25 +19,38 @@ class Liczba_wymierna:
             else:
                 if n and d == None:
                     print('Liczba jest zerem')
-                else:
-                    raise ValueError("WTF!")
         else:
             n = numerator
             d = denominator
+        
+        self._numerator = n
+        self._denominator = d
 
-        self.__numerator = n
-        self.__denominator = d
-
-        self.integer_part, self.numerator, self.denominator = self.convert_to_mixed_num(self.__numerator, self.__denominator)
-
+        if self._numerator != 0 and self._denominator != 0:
+            self.fraction_reduction()
+            self.integer_part, self.numerator, self.denominator = self.convert_to_mixed_num(self._numerator, self._denominator)
+        else:
+            self.integer_part, self.numerator, self.denominator = integer_part, 0, 0
+    
 
     def __str__(self):
-        return "Ulamek niewlasciwy: {}/{} Czesc calkowita: {} czesc ulamkowa: {}/{}".format(
-            self.__numerator,
-            self.__denominator, 
-            self.integer_part, 
-            self.numerator, 
-            self.denominator)
+        if self.integer_part:
+            if self.numerator != 0:
+                return "Ulamek niewlasciwy: {}/{} Czesc calkowita: {} czesc ulamkowa: {}/{}".format(
+                    self._numerator,
+                    self._denominator, 
+                    self.integer_part, 
+                    self.numerator, 
+                    self.denominator)
+            else:
+                return "Ulamek niewlasciwy: {}/{} Liczba calkowita: {}".format(
+                    self.integer_part, 
+                    1, 
+                    self.integer_part)
+        else:
+            return "Ulamek zwykly: {}/{} brak liczby calkowitej".format(
+                self._numerator,
+                self._denominator)
     
     def __repr__(self):
         return "RationalNumber({},{},{})".format(
@@ -52,7 +64,47 @@ class Liczba_wymierna:
         numerator = n - (integer_part * d)
         if n < 0:
             numerator = abs(numerator)
+        if numerator == 0:
+            if integer_part != 0: d = 0
+            else:
+                raise ValueError("Nie mozesz podac tylko mianownika, licznik nie moze byc zerem")
         return integer_part, numerator, d
+
+    def fraction_reduction(self):
+        gcd = math.gcd(self._numerator, self._denominator)
+        self._numerator = int(self._numerator / gcd)
+        self._denominator = int(self._denominator / gcd)
+        return self._numerator, self._denominator
+        
+    def get_complex_fraction(self):
+        return (self._numerator, self._denominator)
+
+    def get_integer_part(self):
+        return self.integer_part
+
+    def get_fration_part(self):
+        return (self.numerator, self.denominator)
+
+    def __eq__(self, other):
+        l1, m1 = self.get_complex_fraction()
+        l2, m2 = other.get_complex_fraction()
+        return l1 == l2 and m1 == m2
+
+    def __gt__(self, other):
+        l1, m1 = self.get_complex_fraction()
+        l2, m2 = other.get_complex_fraction()
+        return l1/m1 > l2/m2
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __lt__(self, other):
+        l1, m1 = self.get_complex_fraction()
+        l2, m2 = other.get_complex_fraction()
+        return l1/m1 < l2/m2
+
+    def __le__(self, other):
+        return self < other or self == other
 
     def __add__(self, other):
         pass
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from Liczba_wymierna import Liczba_wymierna
 
 if __name__ == "__main__":
-    L = Liczba_wymierna(-2, 3, 7)
-    print(L)
-    print(repr(L))
+    L = Liczba_wymierna(-3, 32, 64)
+    print("-----------------------------------------------------")
+    print("1. Wyswietlanie liczby")
+    print("2. Zwracanie formy reprezentacyjnej")
+    print("-----------------------------------------------------")
     
+    print("Liczba: {}".format(L))
+    print("Forma reprezentacyjna: {}".format(repr(L)))
+    
+    print("Ulamek niewlasciwy: {}".format(L.get_complex_fraction()))
+    print("Czesc calkowita: {}".format(L.get_fration_part()))
+    print("Czesc ulamkowa: {}".format(L.get_integer_part()))
+
+    print("-----------------------------------------------------")
+    print("3. Porownywanie liczb")
+    print("-----------------------------------------------------")
+    
+    L1 = Liczba_wymierna(2, 2, 4)
+    L2 = Liczba_wymierna(0, 10, 4)
+    
+    print("Czy liczby: {}{} \nsą równe?: {}".format(
+        "\n\t Liczba 1.: {}".format(L1),
+        "\n\t Liczba 2.: {}".format(L2),
+        'Tak' if L1 == L2 else 'Nie'))
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    
+    L3 = Liczba_wymierna(-2, 3, 7)
+    L4 = Liczba_wymierna(0, -9, 4)
+
+    print("Czy 1. liczba z podanych ponizej: {}{} \njest większa?: {}".format(
+    "\n\t Liczba 1.: {}".format(L3),
+    "\n\t Liczba 2.: {}".format(L4),
+    'Tak' if L3 > L4 else 'Nie'))
+
+    print("-----------------------------------------------------")
