3cd653ccbc5ad1042fc34c115abe37b791b5a750
Paweł Kalicki <kalicki.paul@icloud.com>
2020-10-16 13:26:34 +0200

    Added alternative method for exercise 1

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import numpy as np
 
 def exercise1(a, offset):
+    return np.vstack((
+        np.zeros((offset, a.shape[0]+2*offset+1)), 
+        np.hstack((
+            np.zeros((a.shape[0],offset)),
+            a,
+            np.zeros((a.shape[0],offset)))),
+        np.zeros((offset, a.shape[0]+2*offset+1))
+    ))
+
+def exercise1_1(a, offset):
     result = np.zeros((offset*2 + a.shape[0], offset*2 + a.shape[1]))
     result[offset:a.shape[0]+offset, offset:a.shape[1]+offset] = a
     return result
 
+#TODO: policzyc czasy dla dwoch funkcji
 a = np.ones((4,5))
-offset = 2
+offset = 3
 
 print(a)
 print(exercise1(a, offset))
+print(exercise1_1(a, offset))
