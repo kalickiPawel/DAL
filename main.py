46769d6454aa8dbd883bec52789d80851340b0cb
Paweł <kalicki.paul@icloud.com>
2020-10-22 22:26:51 +0200

    Added function for exercise 2

--- main.py
+++ main.py
@@ -1,24 +1,93 @@
 import numpy as np
+import time
 
-def exercise1(a, offset):
+
+def exercise1(input_matrix, offset_to_add):
+    """
+    This function adding the offset into declared input matrix
+    :param input_matrix: <class 'numpy.ndarray'>
+    :param offset_to_add: <class 'int'>
+    :return: <class 'numpy.ndarray'> -> new matrix with offset
+    """
     return np.vstack((
-        np.zeros((offset, a.shape[0]+2*offset+1)), 
+        np.zeros((offset_to_add, input_matrix.shape[1] + 2 * offset_to_add)),
         np.hstack((
-            np.zeros((a.shape[0],offset)),
-            a,
-            np.zeros((a.shape[0],offset)))),
-        np.zeros((offset, a.shape[0]+2*offset+1))
+            np.zeros((input_matrix.shape[0], offset_to_add)),
+            input_matrix,
+            np.zeros((input_matrix.shape[0], offset_to_add)))),
+        np.zeros((offset_to_add, input_matrix.shape[1] + 2 * offset_to_add))
     ))
 
-def exercise1_1(a, offset):
-    result = np.zeros((offset*2 + a.shape[0], offset*2 + a.shape[1]))
-    result[offset:a.shape[0]+offset, offset:a.shape[1]+offset] = a
+
+# def exercise1_1(a, offset):
+#     '''
+#     Alternative version exercise 1
+#     '''
+#     result = np.zeros((offset*2 + a.shape[0], offset*2 + a.shape[1]))
+#     result[offset:a.shape[0]+offset, offset:a.shape[1]+offset] = a
+#     return result
+
+
+def exercise2(n):
+    """
+    This function generates an output matrix with spiral values
+    :param n: <class 'int'>
+    :return: <class 'numpy.ndarray'> -> new spiral values matrix
+    """
+    result = np.zeros((n, n))
+    w, k, i = n - 1, 0, 1
+
+    while True:
+        while w - 1 >= 0 and result[w - 1, k] == 0:
+            result[w, k] = i
+            w, i = w - 1, i + 1
+        if i >= n ** 2: break
+
+        while k + 1 < n and result[w, k + 1] == 0:
+            result[w, k] = i
+            k, i = k + 1, i + 1
+        if i >= n ** 2: break
+
+        while w + 1 < n and result[w + 1, k] == 0:
+            result[w, k] = i
+            w, i = w + 1, i + 1
+        if i >= n ** 2: break
+
+        while k - 1 >= 0 and result[w, k - 1] == 0:
+            result[w, k] = i
+            k, i = k - 1, i + 1
+        if i >= n ** 2: break
+
+    result[w, k] = i
     return result
 
-#TODO: policzyc czasy dla dwoch funkcji
-a = np.ones((4,5))
-offset = 3
 
-print(a)
-print(exercise1(a, offset))
-print(exercise1_1(a, offset))
+def exercise3():
+    pass
+
+
+def exercise4():
+    pass
+
+
+def exercise5():
+    pass
+
+
+if __name__ == "__main__":
+    print("--------------Exercise_1--------------")
+
+    a = np.ones((4, 5))
+    offset = 2
+
+    print("Input: \n", a)
+    print("Output: \n", exercise1(a, offset))
+    # exercise1_1(a, offset)
+
+    print("--------------Exercise_2--------------")
+    n = 5
+    print(exercise2(n))
+
+    # print("--------------Exercise_3--------------")
+    # print("--------------Exercise_4--------------")
+    # print("--------------Exercise_5--------------")
