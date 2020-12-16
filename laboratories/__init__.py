ba2792093120f2b8676fa1968c36fffe4612958c
Paweł <kalicki.paul@icloud.com>
2020-12-16 13:03:24 +0100

    :art: Changed structure of lab 3

--- laboratories/__init__.py
+++ laboratories/__init__.py
@@ -1,2 +1,3 @@
 from laboratories.jad_1 import FirstLab
 from laboratories.jad_2 import SecondLab
+from laboratories.jad_3 import ThirdLab
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from laboratories import FirstLab
-from laboratories import SecondLab
+from laboratories import FirstLab, SecondLab, ThirdLab
 
 if __name__ == "__main__":
-    # lab1 = FirstLab()
+    lab1 = FirstLab()
     lab2 = SecondLab()
+    lab3 = ThirdLab()
